c05eef10c136bde5bb4f55e9d2fa53d4fc662eed
dmcyk <damian.malarczyk@gmail.com>
2018-04-11 22:03:26 +0200

    update SwiftyCurl

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/crypto.git", .upToNextMajor(from: "2.1.0")),
-        .package(url: "https://github.com/dmcyk/SwiftyCurl.git", .upToNextMajor(from: "0.7.0")),
+        .package(url: "https://github.com/dmcyk/SwiftyCurl.git", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
         .target(
